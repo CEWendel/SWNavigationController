e0ece75f44c742a763f62cc0db4c29f76234a582
Christopher Wendel <chriwend@umich.edu>
2015-01-01 17:18:15 -0500

    Adding table views to example app

--- SWNavigationController/TableViewController.m
+++ SWNavigationController/TableViewController.m
@@ -24,4 +24,22 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"CellIdentifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"Cell %ld", indexPath.row];
+    
+    return cell;
+}
+
 @end
