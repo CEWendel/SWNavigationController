0a3bf9ab84e9b0bc9f1a92a2c93ce32eb8c659a4
Christopher Wendel <chriwend@umich.edu>
2015-01-01 19:37:39 -0500

    Adding shadow path to the to view layer in the interactive transition

--- SWNavigationController/PodFiles/SWPushAnimatedTransitioning.m
+++ SWNavigationController/PodFiles/SWPushAnimatedTransitioning.m
@@ -4,6 +4,7 @@
 //
 //  Created by Christopher Wendel on 12/31/14.
 //  Copyright (c) 2014 Christopher Wendel. All rights reserved.
+//  Based off of 'SlideAnimatedTransitioning' https://github.com/visnup/swipe-left/blob/master/SwipeLeft/SlideAnimatedTransitioning.m
 //
 
 #import "SWPushAnimatedTransitioning.h"
@@ -17,25 +18,32 @@
 
 - (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
 {
-    UIView *containerView = [transitionContext containerView],
-    *fromView = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey].view,
-    *toView   = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey].view;
+    UIView *containerView = [transitionContext containerView];
+    UIView *toView = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey].view;
+    UIView *fromView = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey].view;
     
-    CGFloat width = containerView.frame.size.width;
+    CGFloat containerViewWidth = containerView.frame.size.width;
     UIView *snapshotToView = [toView snapshotViewAfterScreenUpdates:YES];
-    
-    CGRect offsetLeft = fromView.frame;
-    offsetLeft.origin.x = -width/3;
-    
-    CGRect offscreenRight = toView.frame;
-    offscreenRight.origin.x = width;
-    
-    snapshotToView.frame = offscreenRight;
-    snapshotToView.layer.shadowRadius = kSWToLayerShadowRadius;
-    snapshotToView.layer.shadowOpacity = kSWFromLayerShadowOpacity;
     
     [containerView addSubview:snapshotToView];
     
+    // Calculate new frame for fromView
+    CGRect fromViewFinalFrame = fromView.frame;
+    fromViewFinalFrame.origin.x = -containerViewWidth/3.f;
+    
+    // Calculate the new frame for toView snapshot
+    CGRect toViewFinalFrame = toView.frame;
+    toViewFinalFrame.origin.x = containerViewWidth;
+    
+    // Use a shadow path to make rendering during the interactive transition better 
+    snapshotToView.frame = toViewFinalFrame;
+    snapshotToView.layer.shadowRadius = kSWToLayerShadowRadius;
+    snapshotToView.layer.shadowOpacity = kSWFromLayerShadowOpacity;
+    CGRect shadowFrame = toView.layer.bounds;
+    CGPathRef shadowPath = [UIBezierPath bezierPathWithRect:shadowFrame].CGPath;
+    snapshotToView.layer.shadowPath = shadowPath;
+    
+    // Tries to mimic the shadow animation on the default pop animation
     CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"shadowOpacity"];
     anim.fromValue = [NSNumber numberWithFloat:kSWFromLayerShadowOpacity];
     anim.toValue = [NSNumber numberWithFloat:kSWToLayerShadowOpacity];
@@ -45,14 +53,13 @@
     
     [UIView animateWithDuration:[self transitionDuration:transitionContext] delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
         snapshotToView.frame = fromView.frame;
-        
-        fromView.frame = offsetLeft;
-        fromView.layer.opacity = 0.9;
+        fromView.frame = fromViewFinalFrame;
     } completion:^(BOOL finished) {
-        fromView.layer.opacity = 1;
         snapshotToView.layer.shadowOpacity = 0;
         
         [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
+        
+        // If transition was not cancelled, actually add the toView to our view hierarchy
         if (![transitionContext transitionWasCancelled]) {
             [containerView addSubview:toView];
             [snapshotToView removeFromSuperview];
